96797051b1712083ec82313e767d5ff1c6adb438
Angus-Clarke <abclarke07@outlook.com>
2026-08-08 14:32:01 +1000

    Strategies

--- trader_interface/algorithm.py
+++ trader_interface/algorithm.py
@@ -1,6 +1,9 @@
 import numpy as np
 
-# Example Change
+# Constants
+SIZZLE_SAFETY_MARGIN = 0.002 # 1% Increase
+UQ_DOLLAR_WAIT = 20
+UQ_SAFETY_MARGIN = 0.002
 
 # Custom trading Algorithm
 class Algorithm():
@@ -44,6 +47,15 @@ class Algorithm():
         # Display the prices of instruments I want to trade
         for ins in trade_instruments:
             print(f"{ins}: ${self.get_current_price(ins)}")
+
+############################################################################################
+        # UQ Dollar Trading
+        if self.day > UQ_DOLLAR_WAIT:
+            UQ_dollar_avg = 100
+            if self.data["UQ Dollar"][-1]/UQ_dollar_avg > 1 + UQ_SAFETY_MARGIN:
+                desiredPositions["UQ Dollar"] = -positionLimits["UQ Dollar"]
+            elif self.data["UQ Dollar"][-1]/UQ_dollar_avg < 1 - UQ_SAFETY_MARGIN:
+                desiredPositions["UQ Dollar"] = positionLimits["UQ Dollar"]
         
         # If its 10th day or more, then trade
         if self.day >= 10:
@@ -56,15 +68,15 @@ class Algorithm():
                 else: # else, short
                     desiredPositions[ins] = -positionLimits[ins]
 
-            # Thrifted Jeans Trading
+            # Thrifted Jeans Trading (Swing Trading)
             todays_price = self.data["Thrifted Jeans"][-1]
             yesterdays_price = self.data["Thrifted Jeans"][-2]
             if yesterdays_price > todays_price:
                 desiredPositions["Thrifted Jeans"] = positionLimits["Thrifted Jeans"]
-            else: # else, short
+            elif yesterdays_price < todays_price: # else, short
                 desiredPositions["Thrifted Jeans"] = -positionLimits["Thrifted Jeans"]
 
-            # Sausage Sizzle Trading
+            # Sausage Sizzle Trading (Index Trading)
             print(f"{"Sausage Sizzle"}: ${self.get_current_price("Sausage Sizzle")}")
 
             bread_change = self.data["Bread"][-1] / self.data["Bread"][-2]
@@ -78,9 +90,34 @@ class Algorithm():
                 desiredPositions["Sausage Sizzle"] = -positionLimits["Sausage Sizzle"]
             else:
                 desiredPositions["Sausage Sizzle"] = positionLimits["Sausage Sizzle"]
-            # Sausage Sizzle Component Trading (i.e. Bread, Sausage, MenuDash)
-            
 
+            # Sausage Sizzle Component Trading (i.e. Bread, Sausage, MenuDash)
+            if bread_change > sausage_sizzle_change + SIZZLE_SAFETY_MARGIN:
+                desiredPositions["Bread"] = -positionLimits["Bread"]
+            elif bread_change < sausage_sizzle_change - SIZZLE_SAFETY_MARGIN:
+                desiredPositions["Bread"] = positionLimits["Bread"]
+
+            if sausage_change > sausage_sizzle_change + SIZZLE_SAFETY_MARGIN:
+                desiredPositions["Sausage"] = -positionLimits["Sausage"]
+            elif sausage_change < sausage_sizzle_change - SIZZLE_SAFETY_MARGIN:
+                desiredPositions["Sausage"] = positionLimits["Sausage"]
+
+            if menu_dash_change > sausage_sizzle_change + SIZZLE_SAFETY_MARGIN:
+                desiredPositions["MenuDash"] = -positionLimits["MenuDash"]
+            elif menu_dash_change < sausage_sizzle_change - SIZZLE_SAFETY_MARGIN:
+                desiredPositions["MenuDash"] = positionLimits["MenuDash"]
+
+        # Boat Party Ticket Trading
+        if self.day % 365 <= 30:
+            desiredPositions["Boat Party Ticket"] = positionLimits["Boat Party Ticket"]
+        elif 30 < self.day % 365 <= 130:
+            desiredPositions["Boat Party Ticket"] = -positionLimits["Boat Party Ticket"]
+        elif 130 < self.day % 365 <= 190:
+            desiredPositions["Boat Party Ticket"] = positionLimits["Boat Party Ticket"]
+        elif 190 < self.day % 365 <= 300:
+            desiredPositions["Boat Party Ticket"] = -positionLimits["Boat Party Ticket"]
+        elif 300 < self.day % 365:
+            desiredPositions["Boat Party Ticket"] = positionLimits["Boat Party Ticket"]
                                 
                     
         # Display the end of trading day
